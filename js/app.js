b1d91f5cd3d165b5f75a52abafdc5a65e1e02f0e
Edwin Nord <edwin.nord09@gmail.com>
2022-07-01 14:18:23 -0400

    Js Update

--- js/app.js
+++ js/app.js
@@ -1,13 +1,34 @@
 const nav = document.querySelector('.navbar'); 
-const arrowBtn = document.querySelector('.arrow');
+const arrowBtn = document.getElementById('down-arrow');
+const emailSection = document.querySelector('.email');
+const webSection = document.querySelector('.web');
 window.addEventListener('scroll', fixedNav);
 
 function fixedNav() {
     if(window.scrollY > nav.offsetHeight + 150) {
+        emailSection.style.display = 'block'
+        webSection.style.display = 'block'
         nav.classList.add('active')
+        
+       
     } else {
+        emailSection.style.display = 'none'
+        webSection.style.display = 'none'
         nav.classList.remove('active')
     }
 }
+
+
+
+arrowBtn.addEventListener('click', function(e) {
+    if(emailSection.style.display = 'none')
+    if(webSection.style.display = 'none') {
+        emailSection.style.display = 'block';
+        webSection.style.display = 'block';
+    }
+})
+
+
+
 
 
