384eb9075dc5ae57e864c7c188bcafbb8546e2e0
Edwin Nord <edwin.nord09@gmail.com>
2023-01-22 18:55:04 -0500

    Form validation

--- js/app.js
+++ js/app.js
@@ -6,10 +6,6 @@ const serviceAnimation = document.querySelector('.service-title');
 const skills = document.querySelector('.skills');
 const card = document.querySelector('.g-col-6');
 const arrowTop = document.querySelector('.arrow-top')
-const name = document.getElementById('name');
-const messageEmail = document.getElementById('email_id');
-const subject = document.getElementById('subject');
-const message = document.getElementById('message');
 const messageBtn = document.getElementById('btn-form');
 const form = document.querySelector('.contact-form');
 
@@ -74,39 +70,46 @@ function fixedNav() {
 
 // FORM //
 
-function sendEmail() {
 
 
 
-    const params = {
-        from_name: document.getElementById('name').value,
-        email_id: document.getElementById('email_id').value,
-        subject: document.getElementById('subject').value,
-        message: document.getElementById('message').value,
+
+
+
+
+// function sendEmail() {
+
+
+
+//     const params = {
+//         from_name: document.getElementById('name').value,
+//         email_id: document.getElementById('email_id').value,
+//         subject: document.getElementById('subject').value,
+//         message: document.getElementById('message').value,
         
-    }
+//     }
 
-    emailjs.send("service_3xn8b0e", "template_8hedb59", params).then(function (res) {
+//     emailjs.send("service_3xn8b0e", "template_8hedb59", params).then(function (res) {
         
 
 
-        alert('Message sent' + res.status);
-        // successAlert.style.display = "block"
+//         alert('Message sent' + res.status);
+//         // successAlert.style.display = "block"
 
-        // setTimeout(() => {
+//         // setTimeout(() => {
             
-        //     email_id.value = "";
-        //     name.value = "";
-        //     subject.value = "";
-        //     message.value = "";
+//         //     email_id.value = "";
+//         //     name.value = "";
+//         //     subject.value = "";
+//         //     message.value = "";
 
-        // }, 10000)
+//         // }, 10000)
 
 
 
-    })
+//     })
 
-}
+// }
 
 
 
